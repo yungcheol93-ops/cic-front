714b66efc3e89a7dc6ce3fc13c2bc876882daeff
sujin <sujinego3971@gmail.com>
2026-04-18 18:40:15 +0900

    배포 화면 비율 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,35 +10,34 @@ export default function Layout({ children }: PropsWithChildren) {
     const isHome = location.pathname === "/";
 
     return (
-        <div className="w-screen h-screen overflow-hidden">
+        <div className="w-full h-screen overflow-hidden bg-black">
             {/* 모바일 헤더 */}
-            <div className="block md:hidden">
+            <div className="md:hidden h-[60px]">
                 <MobileHeader isHome={isHome} />
             </div>
 
             {/* 데스크탑 레이아웃 */}
             <div className="hidden md:flex w-full h-full text-white">
-
                 {/* 왼쪽 사이드바 */}
-                <aside className="w-[20vw] h-full px-12 py-6 flex flex-col justify-between">
+                <aside className="flex-none w-[300px] h-full px-8 py-6 flex flex-col justify-between">
                     <LeftSidebar isHome={isHome} />
                 </aside>
                 {/* 메인 영역 */}
-                <main className= { "w-[60vw] h-full " +
-                (isHome
-                    ? "overflow-hidden"                    // 메인에서는 스크롤 없음
-                    : "overflow-y-auto no-scrollbar")      // 나머지 페이지: 가운데만 스크롤 + 스크롤바 숨김
-                }>
-                    {children}
+                <main className={`flex-1 h-full ${
+                    isHome ? "overflow-hidden" : "overflow-y-auto no-scrollbar"
+                }`}>
+                    <div className="max-w-[1200px] mx-auto w-full h-full"> {/* 내부 컨텐츠 폭 제한 */}
+                        {children}
+                    </div>
                 </main>
                 {/*오른쪽 사이드바 바닥*/}
-                <footer className="w-[20vw] h-full px-12 py-6 flex flex-col  justify-end">
+                <footer className="flex-none w-[300px] h-full px-8 py-6 flex flex-col justify-end">
                     <RightSideBar isHome={isHome} />
                 </footer>
             </div>
 
             {/* 모바일 컨텐츠 */}
-            <div className="block md:hidden h-[calc(100%-60px)] overflow-y-auto">
+            <div className="md:hidden h-[calc(100vh-60px)] overflow-y-auto">
                 {children}
             </div>
         </div>
